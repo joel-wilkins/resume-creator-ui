ffc1e950901dbf8d145a70cfd277d7674d027ec7
Joel Wilkins <jwilkins88@gmail.com>
2020-04-04 14:07:05 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <HomePage msg="Welcome to Resume Builder" />
+  </div>
+</template>
+
+<script>
+import HomePage from '@/components/HomePage.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HomePage,
+  },
+};
+</script>
